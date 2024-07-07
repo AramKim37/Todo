19188af8e1bfb1d1512583f6960c8ae1da5d0313
AramKim37 <aramkim37@gmail.com>
2024-07-06 20:10:30 -0600

    add css in the layout page for layout

--- app/layout.js
+++ app/layout.js
@@ -13,9 +13,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className="flex flex-col gap-[3rem] min-h-screen">
         <Header />
-        <main>{children}</main>
+        <main className="flex-1">{children}</main>
         <Footer />
       </body>
     </html>
