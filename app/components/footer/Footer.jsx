a4bf626ee83981dbe9fee606a739f21689c26cf4
AramKim37 <aramkim37@gmail.com>
2024-07-08 21:19:28 -0600

    completed footer and header

--- app/components/footer/Footer.jsx
+++ app/components/footer/Footer.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="flex items-center justify-between px-20 py-8 border-2 border-t-gray-200">
+      <div>
+        <h1>@ 2024 All Right Reserved</h1>
+      </div>
+      <ul className="flex gap-5">
+        <li>Terms</li>
+        <li>Privacy</li>
+        <li>Policy</li>
+        <li>Cookies Policy</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Footer;
--- app/page.js
+++ app/page.js
@@ -1,5 +1,9 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <h1>Home page</h1>;
+  return (
+    <h1 className="flex items-center justify-center text-xl font-bold">
+      Home page
+    </h1>
+  );
 }
