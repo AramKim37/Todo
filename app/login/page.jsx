872d1cef68e604abd580b65972e98f32ae75c1ad
AramKim37 <aramkim37@gmail.com>
2024-07-09 19:42:27 -0600

    building login page

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Login = () => {
-  return <div>login page</div>;
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-full p-10">
+      <div>
+        <h1>LOGIN</h1>
+      </div>
+      <form className="flex flex-col gap-5 w-[50%]">
+        <input placeholder="Email" className="border-2 p-5"></input>
+        <input placeholder="Password" className="border-2 p-5"></input>
+        <button className="border-2 bg-pink-500 text-white p-5 rounded-xl">
+          Login
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
