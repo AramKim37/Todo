59945732b5bb63ac9bc22ea4ffffd756e89bdc0f
AramKim37 <aramkim37@gmail.com>
2024-07-10 18:41:42 -0600

    completed login page's ui

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,17 +1,29 @@
+import Link from "next/link";
 import React from "react";
 
 const Login = () => {
   return (
     <div className="flex flex-col items-center justify-center w-full h-full p-10">
-      <div>
-        <h1>LOGIN</h1>
+      <div className="py-5">
+        <h1 className="font-bold text-2xl">LOGIN</h1>
       </div>
       <form className="flex flex-col gap-5 w-[50%]">
-        <input placeholder="Email" className="border-2 p-5"></input>
-        <input placeholder="Password" className="border-2 p-5"></input>
+        <input placeholder="Email" className="border-2 p-5 rounded-xl"></input>
+        <input
+          placeholder="Password"
+          className="border-2 p-5 rounded-xl"
+        ></input>
         <button className="border-2 bg-pink-500 text-white p-5 rounded-xl">
           Login
         </button>
+        <div className="flex items-center justify-center">
+          <h1>
+            Don`t have an account?
+            <Link href="/register" className="underline px-3 font-bold">
+              Register
+            </Link>
+          </h1>
+        </div>
       </form>
     </div>
   );
