4447ab8682642884516b65dee112c00ca41fdbd6
AramKim37 <aramkim37@gmail.com>
2024-07-12 19:17:08 -0600

    building backend section

--- app/register/page.jsx
+++ app/register/page.jsx
@@ -1,7 +1,14 @@
+"use client";
+
 import Link from "next/link";
 import React from "react";
+import { useState } from "react";
 
 const Register = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  console.log(username);
   return (
     <div className="flex flex-col items-center justify-center w-full h-full p-10">
       <div className="py-5">
@@ -11,12 +18,10 @@ const Register = () => {
         <input
           placeholder="Username"
           className="border-2 p-5 rounded-xl"
-        ></input>
-        <input placeholder="Email" className="border-2 p-5 rounded-xl"></input>
-        <input
-          placeholder="Password"
-          className="border-2 p-5 rounded-xl"
-        ></input>
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input placeholder="Email" className="border-2 p-5 rounded-xl" />
+        <input placeholder="Password" className="border-2 p-5 rounded-xl" />
         <button className="border-2 bg-pink-500 text-white p-5 rounded-xl">
           Register
         </button>
