18033f7eeddf14c39356f276c743a9790e11fbf9
AramKim37 <aramkim37@gmail.com>
2024-07-23 21:11:17 -0600

    install bcrypt for password and add functions in auth page

--- auth.js
+++ auth.js
@@ -1,14 +1,30 @@
 import NextAuth from "next-auth";
 import { authConfig } from "./authconfig";
 import Credentials from "next-auth/providers/credentials";
+import connectToDB from "./utils/connectToDB";
+import User from "./models/userModel";
+import bcrypt from "bcrypt";
+import { log } from "console";
 
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
   providers: [
     Credentials({
       async authorize(credentials) {
-        console.log("these are the credential: ", credentials);
-        return null;
+        try {
+          await connectToDB();
+          const user = await User.findOne({ email: credentials.email });
+          if (!user) return null;
+          const isCorrect = await bcrypt.compare(
+            credentials.password,
+            user.password
+          );
+          if (!isCorrect) return null;
+          return user;
+        } catch (error) {
+          console.log("error: ", error);
+          return null;
+        }
       },
     }),
   ],
