1bd6560ca0fd8d8ba97f4968587ba08b38fa0940
AramKim37 <aramkim37@gmail.com>
2024-07-31 22:16:34 -0600

    add profile image

--- app/components/sidebar/Sidebar.jsx
+++ app/components/sidebar/Sidebar.jsx
@@ -1,10 +1,19 @@
 "use client";
+import Image from "next/image";
 import React from "react";
 
 const Sidebar = () => {
   return (
     <div className="w-[15rem] bg-neutral-800 border border-white-200 rounded-xl">
-      Sidebar
+      <div>
+        <div>
+          <Image width={70} height={70} src="/dotory.jpeg" alt="profile" />
+        </div>
+      </div>
+      <h1>
+        <span>Aram</span>
+        <span>Kim</span>
+      </h1>
     </div>
   );
 };
