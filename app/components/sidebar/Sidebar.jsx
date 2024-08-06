5c3108b245199307fbb86d5290161bcd0f46bd09
AramKim37 <aramkim37@gmail.com>
2024-08-05 21:55:53 -0600

    install react icons

--- app/components/sidebar/Sidebar.jsx
+++ app/components/sidebar/Sidebar.jsx
@@ -1,20 +1,30 @@
 "use client";
 import Image from "next/image";
 import React from "react";
+import { GiNotebook } from "react-icons/gi";
 
 const Sidebar = () => {
   return (
     <div className="w-[15rem] bg-neutral-800 border border-white-200 rounded-xl">
-      <div>
-        <div>
-          <Image width={70} height={70} src="/dotory.jpeg" alt="profile" />
-        </div>
+      <div className="flex p-5 w-max">
+        <Image
+          width={70}
+          height={70}
+          src="/dotory.jpeg"
+          alt="profile"
+          className="rounded-full"
+        />
+        <h1 className="w-full flex items-center">
+          <span>Aram</span>
+          <span>Kim</span>
+        </h1>
       </div>
-      <h1>
-        <span>Aram</span>
-        <span>Kim</span>
-      </h1>
-      <h1>Home</h1>
+      <ul className="">
+        <li className="flex ">
+          <GiNotebook />
+          <h1>Note</h1>
+        </li>
+      </ul>
     </div>
   );
 };
